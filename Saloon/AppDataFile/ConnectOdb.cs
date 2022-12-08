653a59473300fe255639a794605d3f55d7e79aeb
IUTA <vasavaseckin801@gmail.com>
2022-12-08 21:18:29 +0300

    Saloon

---
+++ Saloon/AppDataFile/ConnectOdb.cs
@@ -0,0 +1,14 @@
+﻿using Saloon.AppDataFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saloon.AppDataFile
+{
+    internal class ConnectOdb
+    {
+        public static BeautyShopEntities conObj;
+    }
+}
---
+++ Saloon/Pages/PageService.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Saloon.AppDataFile;
+
+namespace Saloon.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageService.xaml
+    /// </summary>
+    public partial class PageService : Page
+    {
+        public PageService()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateData;
+            timer.Start();
+        }
+
+        public void UpdateData(object sender, object e)
+        {
+            var HistoryService = ConnectOdb.conObj.Service.ToList();
+            ListService.ItemsSource = HistoryService;
+            ListService.ItemsSource = ConnectOdb.conObj.Service.Where(x => x.Title.StartsWith(TxtSearch.Text) || x.Description.StartsWith(TxtSearch.Text)).ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            FrameObj.frameMain.Navigate(new PageEditNew((sender as Button).DataContext as Service));
+        }
+
+        private void BtnSalehistory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
